6751802070600d42806012e2aab04ff0fe7d70d5
VictorShielo <82816338+sirMasterEgg@users.noreply.github.com>
2024-08-04 23:50:02 +0700

    try fix usestate error

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,26 @@ export default defineConfig({
             if (id.includes("react")) {
               return "vendor_react";
             }
+            if (id.includes("zustand")) {
+              return "vendor_zustand";
+            }
+            if (id.includes("aos")) {
+              return "vendor_aos";
+            }
+            if (id.includes("react-router-dom")) {
+              return "vendor_react_router";
+            }
+            if (id.includes("@emailjs/browser")) {
+              return "vendor_emailjs";
+            }
+            if (id.includes("vanta")) {
+              return "vendor_vanta";
+            }
+            if (id.includes("react-dom")) {
+              return "vendor_react_dom";
+            }
 
-            return "vendor"; // all other package goes here
+            return "vendor_" + id; // all other package goes here
           }
         },
       },
